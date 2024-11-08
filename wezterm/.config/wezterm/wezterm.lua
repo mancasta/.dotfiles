92f5f5c4cbf4f0c99606acb1f4575b4cab7fba13
mancasta <mancasta@cisco.com>
2024-11-08 09:14:42 -0500

    refactor(wezterm): remove comments

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -43,35 +43,18 @@ config.tab_bar_at_bottom = true
 config.max_fps = 120
 config.animation_fps = 30
 
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-
 -- color theme config
 config.color_scheme = 'Malcak'
 config.use_fancy_tab_bar = true
 
 -- tab bar config
 config.window_frame = {
-    -- The font used in the tab bar.
-    -- Roboto Bold is the default; this font is bundled
-    -- with wezterm.
-    -- Whatever font is selected here, it will have the
-    -- main font setting appended to it to pick up any
-    -- fallback fonts you may have used there.
     font = wezterm.font {
         family = 'Roboto',
         weight = 'Bold'
     },
-
-    -- The size of the font in the tab bar.
-    -- Default to 10.0 on Windows but 12.0 on other systems
     font_size = 16.0,
-
-    -- The overall background color of the tab bar when
-    -- the window is focused
     active_titlebar_bg = '#333333',
-
-    -- The overall background color of the tab bar when
-    -- the window is not focused
     inactive_titlebar_bg = '#333333'
 }
 
