e1bfa407483dee8e8d0a98b3512a57e526d0dce9
mancasta <mancasta@cisco.com>
2024-12-09 11:35:39 -0500

    feat(zsh): update zsh variables and secrets

--- zsh/.config/zsh/.zvariables.zsh
+++ zsh/.config/zsh/.zvariables.zsh
@@ -1,7 +1,9 @@
 # env variables
 export AWS_DEFAULT_PROFILE="strln"
+export GIT_SSH_COMMAND="ssh -i /Users/mancasta/.ssh/id_ed25519"
 
 # env secrets
 export JIRA_TOKEN="$(cat ~/.secrets/jira-token)"
 export PD_TOKEN="$(cat ~/.secrets/pagerduty-token)"
-export GITHUB_TOKEN="$(cat ~/.secrets/gh-token)"
+export GH_PAT="$(cat ~/.secrets/ghc-token)"
+export GH_SOURCE_PAT="$(cat ~/.secrets/ghes-token)"
