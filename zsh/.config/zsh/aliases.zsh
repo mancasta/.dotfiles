fdf2c764a7ec2464a0344a91e6db8d7ef748446c
mancasta <mancasta@cisco.com>
2026-09-23 17:52:06 -0500

    feat(zsh): add new preprod env aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -5,12 +5,14 @@ alias lln='gls -Alvh --group-directories-first --color'
 # scheduler api aliases
 alias scheduler-dev='scheduler-cli -H "scheduler.vm.d1.strln.net:32000" --key "$(get-token)"'
 alias scheduler-stage='scheduler-cli -H "scheduler.vm.s1.strln.net" --key "$(get-token)"'
+alias scheduler-preprod='scheduler-cli -H "scheduler-preprod.vm.strln.net" --key "$(get-token)"'
 alias scheduler-prod='scheduler-cli -H "scheduler.vm.strln.net" --key "$(get-token)"'
 
 # streamline aliases
 alias sl-container-prod='sl container cluster configure --cluster-id="elzkfld90z4zx52juhp0dtd1z"'
 alias sl-container-stage='sl container cluster configure --cluster-id="6r7wy3vwemgtj9buh670toju6"'
 alias sl-aws-prod='sl aws session generate --account-id "632748658429" --role-name "owner" --profile "slvm_prod"'
+alias sl-aws-preprod='sl aws session generate --account-id "065148239691" --role-name "owner" --profile "slvm_preprod"'
 alias sl-aws-stage='sl aws session generate --account-id "612669989884" --role-name "owner" --profile "slvm_stage"'
 alias sl-aws-dev='sl aws session generate --account-id "495613368115" --role-name "owner" --profile "slvm_dev"'
 alias token='sl iam token refresh'
